3a0f96bdc607fc419627536d09b34330cab54797
unknown <hoangdai9895@gmail.com>
2019-04-24 14:09:34 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.get('/', (req, res) => res.send('BUI HAI YEN'));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
