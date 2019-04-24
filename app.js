1d95bfe942121282aea3ecc17a2d4b71b768e6e6
unknown <hoangdai9895@gmail.com>
2019-04-24 15:51:26 +0700

    add google auth

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const mongoose = require("mongoose");
+const passport = require('passport');
+
+// Route
+const auth = require('./routes/auth');
+
+// passport config
+require('./config/passport')(passport)
 
 const app = express();
 
 app.get('/', (req, res) => res.send('BUI HAI YEN'));
+app.use('/auth', auth)
 
 const port = process.env.PORT || 5000;
 
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://admin:buihaiyen@video-idea-hiuts.gcp.mongodb.net/StoryBook?retryWrites=true',
+    googleClientID: '610003487494-rdnl75456t39r6ppe5a1q1fd5n0ucmlp.apps.googleusercontent.com',
+    googleCLientSecret: '2ES-5hZlzrYUECCS5USdI0zL'
+}
